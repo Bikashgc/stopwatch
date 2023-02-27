78d7cd7cc10eae08a957ed6c31e9a86d14e36245
Bikash GC <bikashgc937@gmail.com>
2023-02-27 21:13:22 +0545

    designed simple ui of stopwatch

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +13,53 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stop Watch'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CupertinoButton(
+              padding: EdgeInsets.all(0),
+              onPressed: () {},
+              child: Container(
+                height: 250,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 3,
+                  ),
+                ),
+                child: Text(
+                  "00:00:000",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            MaterialButton(
+              color: Colors.purple,
+              textColor: Colors.white,
+              child: Text(
+                "Reset",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
